aacd9f3951b344a6012467a71adae4d52ee3abd8
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-28 15:36:30 -0600

    initial commit. utilizes steve's gamepack with the addition of one block.js file

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: {
+    main: "./lib/index.js",
+    test: "mocha!./test/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
+  }
+};
